89aa6bc18c4d86b3eb5d951f9d40dd1248cd8e77
Cláudio Sá de Castro <claudio@hiades.com.br>
2020-01-20 17:21:12 -0300

    Add feature to save process ID (to text file) when application starts.

--- src/Startup.js
+++ src/Startup.js
@@ -8,8 +8,10 @@
 // References to external code
 //
 // Internal node modules
-//import util from 'util';
+import fs from 'fs';
+import path from 'path';
 // Third-party node modules
+import config from 'config';
 import Future from 'fibers/future';
 
 // References code in other (Catenis Off-Chain Server) modules
@@ -22,11 +24,36 @@ import {IpfsRepo} from './IpfsRepo';
 import {ClientNotification} from './ClientNotification';
 import {RestApi} from './RestApi';
 
+// Config entries
+const startupConfig = config.get('startup');
+
+// Configuration settings
+const cfgSettings = {
+    pidFilename: startupConfig.get('pidFilename')
+};
+
+
+// Definition of module (private) functions
+//
+
+function saveProcessId() {
+    fs.writeFile(path.join(global.CTN_OC_SVR_ROOT_DIR, cfgSettings.pidFilename), process.pid.toString(), (err) => {
+        if (err) {
+            // Error recording process ID
+            CtnOCSvr.logger.ERROR('Error recording process ID.', err);
+        }
+    });
+}
+
+
 // Module code
 //
 
 Future.task(function mainTask() {
     CtnOCSvr.logger.TRACE('Starting application');
+    // Record ID of current process
+    saveProcessId();
+
     Application.initialize();
     Database.initialize();
     CtnNameService.initialize();
